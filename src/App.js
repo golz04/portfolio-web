7b9bb34a5fc0635258b3dd921c9a0a9e1938b427
golz04 <muhammadhaidar862@gmail.com>
2022-04-16 15:33:35 +0700

    section intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import Header from './components/header';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 }
